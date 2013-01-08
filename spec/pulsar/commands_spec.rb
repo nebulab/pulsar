948313458f2a2fd8e8dd3af9f024c2493b23c78a
Matteo Latini <mtylty@gmail.com>
2013-01-08 10:06:21 +0100

    adds some tests

--- spec/pulsar/commands_spec.rb
+++ spec/pulsar/commands_spec.rb
@@ -1,18 +1,57 @@
 require 'spec_helper'
 
-describe Pulsar::MainCommand do
+describe Pulsar do
   include OutputCapture
 
-  context "with required arguments" do
-    before do
-      @pulsar = Pulsar::CapCommand.new("cap")
-      @pulsar.parse(%w(-c repo.com project development))
+  context "MainCommand" do
+    let(:pulsar) { Pulsar::MainCommand.new("") }
+
+    context "--version option" do
+      before do
+        begin
+          pulsar.parse(["--version"])
+        rescue SystemExit => e
+          @system_exit = e
+        end
+      end
+
+      it "shows version" do
+        stdout.should include(Pulsar::VERSION)
+      end
+
+      it "exits with a zero status" do
+        @system_exit.should_not be_nil
+        @system_exit.status.should == 0
+      end
     end
 
-    it "should set variables correctly" do
-      @pulsar.conf_repo.should == "repo.com"
-      @pulsar.application.should == "project"
-      @pulsar.environment.should == "development"
+    context "subcommands" do
+      it "should be cap and list" do
+        help = pulsar.help
+        help.should =~ /Subcommands:/
+        help.should =~ /cap/
+        help.should =~ /list/
+      end
+    end
+  end
+
+  context "CapCommand" do
+    let(:pulsar) { Pulsar::CapCommand.new("cap") }
+
+    context "--conf-repo option" do
+      it "is required" do
+        expect { pulsar.parse([""]) }.to raise_error(Clamp::UsageError)
+      end
+    end
+
+    context "with required args" do
+      it "sets variables correctly" do
+        pulsar.parse(%w(-c repo.com project development))
+
+        pulsar.conf_repo.should == "repo.com"
+        pulsar.application.should == "project"
+        pulsar.environment.should == "development"
+      end
     end
   end
 end
