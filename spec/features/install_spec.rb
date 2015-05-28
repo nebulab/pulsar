4b826258722a296a76199a2bdf62a401a79b05ee
Matteo Latini <mtylty@gmail.com>
2015-05-28 14:14:11 +0200

    adds a couple of feature specs

--- spec/features/install_spec.rb
+++ spec/features/install_spec.rb
@@ -9,21 +9,40 @@ RSpec.feature 'Install' do
     is_expected.not_to match(/Could not find command/)
   end
 
-  context 'creates a directory named pulsar-conf' do
-    scenario 'with the basic pulsar configuration repository'
+  context 'when succeeds' do
+    scenario 'says it out loud' do
+      is_expected.to match(/Successfully created intial repo!/)
+    end
 
-    scenario 'inside the current directory by default' do
-      expect { subject }
-        .to change { File.exist?('./pulsar-conf') }.from(false).to(true)
+    context 'creates a directory named pulsar-conf' do
+      scenario 'with the basic pulsar configuration repository'
+
+      scenario 'inside the current directory by default' do
+        expect { subject }
+          .to change { File.exist?('./pulsar-conf') }.from(false).to(true)
+      end
+    end
+
+    context 'creates a directory named as the passed argument' do
+      let(:arguments) { './my-dir' }
+
+      scenario 'if passed as an argument' do
+        expect { subject }
+          .to change { File.exist?('./my-dir') }.from(false).to(true)
+      end
     end
   end
 
-  context 'creates a directory named as the passed argument' do
-    let(:arguments) { './my-dir' }
+  context 'when fails' do
+    let(:arguments) { '/pulsar-conf' }
 
-    scenario 'if passed as an argument' do
+    scenario 'says it out loud' do
+      is_expected.to match(/Failed to create intial repo./)
+    end
+
+    scenario 'does not create a directory' do
       expect { subject }
-        .to change { File.exist?('./my-dir') }.from(false).to(true)
+        .not_to change { File.exist?('./my-dir') }.from(false)
     end
   end
 end
