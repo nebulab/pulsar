ba9ac46c3068531c62a3878b3ad08931b8d1005e
Flavio Auciello <flavioauciello@nebulab.it>
2017-04-14 19:17:00 +0200

    Tests ExtendedInteractor

--- lib/pulsar/extended_interactor.rb
+++ lib/pulsar/extended_interactor.rb
@@ -4,10 +4,12 @@ module Pulsar
       def validate_context_for(*keys)
         keys.each do |key|
           method_name = "validate_context_for_#{key}"
-          define_method method_name.to_sym do
-            if block_given?
+          if block_given?
+            define_method method_name.to_sym do
               validate_context_for key, &Proc.new
-            else
+            end
+          else
+            define_method method_name.to_sym do
               validate_context_for key
             end
           end
@@ -25,7 +27,7 @@ module Pulsar
 
     def validate_context_for(key)
       error_msg = "Invalid context: #{key} is not properly set"
-      if block_given? && !yield(context.send(key))
+      if block_given? && !yield(context.send(key), context)
         context.fail! error: Pulsar::ContextError.new(error_msg)
       elsif context.send(key).nil?
         context.fail! error: Pulsar::ContextError.new(error_msg)
